f67035374358ade35ebb892e39ca720270967b54
thematkinson <markatkinson0@gmail.com>
2016-08-23 16:25:18 +0200

    Initial Commit

---
+++ TextJustificationLib/Deploy.sql
@@ -0,0 +1,27 @@
+﻿-- First, drop everything in backwards order
+IF  EXISTS (SELECT * FROM sys.procedures procs WHERE procs.name = N'GetJustifiedText')
+	DROP PROCEDURE [dbo].[GetJustifiedText]
+GO
+GO
+ 
+IF  EXISTS (SELECT * FROM sys.assemblies asms WHERE asms.name = N'TextJustificationLib')
+    DROP ASSEMBLY [TextJustificationLib]
+GO
+IF  EXISTS (SELECT * FROM sys.assemblies asms WHERE asms.name = N'FSCore')
+    DROP ASSEMBLY [FSCore]
+GO
+ 
+-- Now, create everything in forwards order
+CREATE ASSEMBLY FSCore FROM 'C:\Users\mark.atkinson\Source\Repos\TextJustificationLib\TextJustificationLib\bin\Debug\FSharp.Core.dll' WITH PERMISSION_SET = UNSAFE
+GO
+CREATE ASSEMBLY SqlClr FROM 'C:\Users\mark.atkinson\Source\Repos\TextJustificationLib\TextJustificationLib\bin\Debug\TextJustificationLib.dll'
+GO
+ 
+-- External name is [SqlAssemblyName].[Full typename].[Method name]
+CREATE PROCEDURE [dbo].[GetJustifiedText]
+	@text [nvarchar](max),
+	@line_width [int]
+WITH EXECUTE AS CALLER
+AS
+EXTERNAL NAME [TextJustificationLib].[TextJustificationLib.SqlClrQuery].[GetJustifiedText]
+GO
---
+++ TextJustificationLib/JustificationTask.fs
@@ -0,0 +1,75 @@
+﻿namespace TextJustificationLib
+
+open System
+open System.IO
+open System.Data.Sql
+open System.Data.SqlTypes
+open System.Data.SqlClient
+open Microsoft.SqlServer.Server
+
+module JustificationTask =
+
+    let rec create_unjustified_list (string_list:List<string>) (num_left:int) (max_num:int) (result:List<int*string>) =
+      match string_list with
+      | head::tail when head.Length + 1 < num_left && tail.Length > 0 -> 
+        let (row_num, current_string)::result_tail = 
+          match result with
+          | head::tail -> result
+          | _ -> [(0, "")]
+        let new_result = current_string + head + " "
+        create_unjustified_list tail (num_left - head.Length - 1) max_num ((row_num+1, new_result)::result_tail) 
+      | head::tail when head.Length + 1 < num_left -> 
+        let (row_num, current_string)::result_tail = 
+          match result with
+          | head::tail -> result
+          | _ -> [(0, "")]
+        let new_result = current_string + head
+        create_unjustified_list tail (num_left - head.Length - 1) max_num ((row_num+1, new_result)::result_tail) 
+      | head::tail when head.Length + 1 >= num_left ->
+        let (row_num, string)::result_tail = result
+        create_unjustified_list (head::tail) max_num max_num ((row_num+1, "")::result)
+      | [] -> result 
+      
+    let rec new_sentence_builder word_list count result =
+      match word_list with
+        | head::tail when count > word_list.Length && count > 0 && tail.Length > 0  ->
+          new_sentence_builder tail (count-2) (String.Concat(result, head, "   "))
+        | head::tail when count > word_list.Length && count > 0 && tail.Length = 0  ->
+          new_sentence_builder tail (count-1) (String.Concat(result, head, " "))
+        | head::tail when count = word_list.Length && count > 0 && tail.Length = 0  ->
+          new_sentence_builder tail (count-1) (String.Concat(result, " ", head))
+        | head::tail when count > 0 && tail.Length > 0 -> new_sentence_builder tail (count-1) (String.Concat(result, head, "  "))
+        | head::tail when count = 0 && tail.Length > 0 -> new_sentence_builder tail 0 (String.Concat(result, head, " "))
+        | head::tail when count = 0 && tail.Length = 0 -> new_sentence_builder tail 0 (String.Concat(result, head, ""))
+        | head::tail when count = 0 -> new_sentence_builder tail 0 (String.Concat(result, head))
+        | [] when count > 0 -> new_sentence_builder [] (count-1) (String.Concat(result, " "))
+        | [] when count = 0 -> result + System.Environment.NewLine
+    
+    let rec justify_list unjustified_list (result:string) (max_num:int) =
+      match unjustified_list with
+      | head::tail -> 
+        let row_num, (sentence:string) = head
+        let remainder = max_num - sentence.TrimStart().Length
+        justify_list tail (new_sentence_builder (sentence.Split(' ')|> Array.toList |> List.filter(fun x -> x.Length > 0)) remainder result) max_num
+      | [] -> result
+  
+
+type SqlClrQuery =
+   [<SqlProcedure()>]
+   static member GetJustifiedText (text:string) (line_width:int) =
+     let string_list = text.Split(' ') |> Array.toList
+     let res = JustificationTask.create_unjustified_list string_list line_width line_width []
+     let revList = List.rev res
+     let revListFinal = revList |> List.filter(fun (x, string) -> string.Length = 40) 
+     let final_string = JustificationTask.justify_list revList "" line_width
+     SqlContext.Pipe.Send(final_string)
+
+     let sqlCom = SqlCommand(String.Concat("Select '",final_string, "'"))
+     SqlContext.Pipe.ExecuteAndSend sqlCom
+     
+     
+
+     
+
+     
+     
